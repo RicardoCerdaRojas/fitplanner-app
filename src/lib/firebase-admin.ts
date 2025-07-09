4561a81212cd938751c73955121c4b71961a5d24
Ricardo Cerda <rdocerda@gmail.com>
2025-07-09 00:40:58 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,11 +1,7 @@
 import * as admin from 'firebase-admin';
 
 if (!admin.apps.length) {
-  try {
-    admin.initializeApp();
-  } catch (error: any) {
-    console.error('Firebase admin initialization error', error.stack);
-  }
+  admin.initializeApp();
 }
 
 export const adminDb = admin.firestore();
