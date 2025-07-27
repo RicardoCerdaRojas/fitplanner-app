e76d0159651abfbb4954f136878bff8a5e52a861
Ricardo Cerda <rdocerda@gmail.com>
2025-07-27 02:49:35 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -11,7 +11,7 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
@@ -19,13 +19,5 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
 const rtdb = getDatabase(app); // Realtime Database for presence
-
-
-export const cleanUpSession = async (uid: string) => {
-    if (!uid) return;
-    const workoutSessionRtdbRef = ref(rtdb, `workoutSessions/${uid}`);
-    await deleteDoc(doc(db, 'workoutSessions', uid));
-    await set(workoutSessionRtdbRef, null); // Remove from RTDB
-}
 
 export { app, auth, db, storage, rtdb };
